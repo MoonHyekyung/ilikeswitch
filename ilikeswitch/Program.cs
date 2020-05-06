2161df4db3817c896be40fd6654e64b3357625b7
MoonHyekyung <moonsoft02@gmail.com>
2020-05-07 00:09:58 +0900

    switch case 문 사용

--- ilikeswitch/Program.cs
+++ ilikeswitch/Program.cs
@@ -10,7 +10,23 @@ namespace ilikeswitch
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("학년을 입력하세요: ");
+            int level = int.Parse(Console.ReadLine());
+            int score = 0;
+            switch (level)
+            {
+                case 1:
+                    score = 12;
+                    break;
+                case 2:
+                case 4:
+                    score = 18;
+                    break;
+                case 3:
+                    score = 10;
+                    break;
+            }
+            Console.WriteLine("수강해야 하는 전공 학점: " + score + "학점");
         }
     }
 }
